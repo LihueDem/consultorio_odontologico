c638f57afa362dba2f75fabe5f68dc7d4ec8a7f3
Lihue <lihue.demelchiorre@gmail.com>
2023-12-11 15:30:21 -0300

    Terminando de componentizar CustomNewSection

--- src/componentes/section/CustomNewCard.jsx
+++ src/componentes/section/CustomNewCard.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 
 const CustomNewCard = ({ dataNovedades }) => {
+  console.log(dataNovedades);
   return (
-    <>
-      <div className="feature col ">
-        {dataNovedades.map((novedades) => (
-          <div>
-            <h3 className="fs-2" key={novedades.id}>
-              {novedades.titulo}
-            </h3>
-            <p>{novedades.descripcion}</p>
-          </div>
-        ))}
-        <a href="#" className="text-info">
-          Call to action
-        </a>
-      </div>
-    </>
+    <ul>
+      {dataNovedades.map((nov) => (
+        <div key={nov.id}>
+          <h3 className="fs-2">{nov.title}</h3>
+          <p>{nov.description}</p>
+
+          <a href="#" className="text-info">
+            Call to action
+          </a>
+        </div>
+      ))}
+    </ul>
   );
 };
 
--- src/componentes/section/CustomNewSection.jsx
+++ src/componentes/section/CustomNewSection.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import CustomNewCard from "./CustomNewCard";
-import { novedadesData } from "../../data/Novedades";
+import CustomNewCard from "./CustomNewCard.jsx";
+import { novedadesData } from "../../data/Novedades.js";
+
 const CustomNewSection = () => {
+  console.log;
   return (
     <>
       <div
@@ -11,10 +13,12 @@ const CustomNewSection = () => {
       >
         <div className="container">
           <h2 className="pb-2 border-bottom ">NOVEDADES</h2>
-          <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <CustomNewCard dataNovedades={novedadesData} />
-            {/* <CustomNewCard />
-            <CustomNewCard /> */}
+          <div className="feature col ">
+            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+              <div className="row">
+                <CustomNewCard dataNovedades={novedadesData} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
